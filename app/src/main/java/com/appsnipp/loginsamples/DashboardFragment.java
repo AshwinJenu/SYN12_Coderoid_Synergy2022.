602d417f681c1f2df050008ac1f450a746892ccc
Ashwin <ashwinjenu2002@gmail.com>
2022-10-22 15:47:57 +0530

    before api

--- app/src/main/java/com/appsnipp/loginsamples/DashboardFragment.java
+++ app/src/main/java/com/appsnipp/loginsamples/DashboardFragment.java
@@ -1,6 +1,8 @@
 package com.appsnipp.loginsamples;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,9 @@ import androidx.fragment.app.Fragment;
 public class DashboardFragment extends Fragment {
 
     Button payment;
+    Button clg_id;
+    Button sem_rep;
+    Button aadhaar;
 
     @Nullable
     @Override
@@ -19,7 +24,21 @@ public class DashboardFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_dashboard,container,false);
 
-        payment = (Button) view.findViewById(R.id.payment);
+        payment = (Button) view.findViewById(R.id.payment_btn);
+        clg_id = (Button) view.findViewById(R.id.clg_id_btn);
+        sem_rep = (Button) view.findViewById(R.id.sem_rep_btn);
+        aadhaar = (Button) view.findViewById(R.id.aadhaar_btn);
+
+        clg_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("application/pdf");
+
+
+            }
+        });
 
         payment.setOnClickListener(new View.OnClickListener() {
             @Override
